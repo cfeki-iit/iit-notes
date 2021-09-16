9a821f921d005c7c91a666837299569b7bb4a363
Chedi Feki <chedi.feki@iit.ens.tn>
2021-09-16 15:49:44 +0100

    vault backup: 2021-09-16 15:49:44
    Affected files:
    "Licence/S5/Developpement d'applications R\303\251parties/Ressources/chapitre1.pdf"
    "Licence/S5/Developpement d'applications R\303\251parties/TP/TP1/Exercice2/Etudiant.java"
    "Licence/S5/Developpement d'applications R\303\251parties/TP/TP1/Exercice2/ListEtudiant.java"
    "Licence/S5/Developpement d'applications R\303\251parties/TP/TP1/Exercice2/Matiere.java"
    "Licence/S5/Developpement d'applications R\303\251parties/TP/TP1/Exercice2/testExercice2.java"

---
+++ Licence/S5/Developpement d'applications Réparties/TP/TP1/Exercice2/Etudiant.java
@@ -0,0 +1,29 @@
+package Exercice2;
+
+public class Etudiant {
+    private String nom;
+    private String adresse;
+
+    public Etudiant(String nom, String adresse){
+        this.nom=nom;
+        this.adresse=adresse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    
+}
---
+++ Licence/S5/Developpement d'applications Réparties/TP/TP1/Exercice2/testExercice2.java
@@ -0,0 +1,8 @@
+package Exercice2;
+
+public class testExercice2 {
+    public static void main(String[] args){
+        
+    }
+    
+}
